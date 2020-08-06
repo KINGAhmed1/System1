d808a910f8cda63eede3d261d2776f1ed234f8f3
Kill3rPal <68537603+Kill3rPal@users.noreply.github.com>
2020-08-06 14:12:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,28 @@ client.on('ready', () => {
 client.user.setActivity('!help', { type: 'PLAYING' })
 });
 
+if (message.content.startsWith(prefix + "warn")) {
+    if (!message.channel.guild)
+      return message.channel
+        .send("THISC  COMMAND IS ONLY FOR SERVERS")
+        .then(m => m.delete(7000));
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+      return message.channel.send("sorry but u dont have ``MANAGE_MESSAGES`` prems");
+  let user = message.guild.members.get(message.content.split(' ')[1]) || message.mentions.members.first();
+    let warn = new Discord.RichEmbed()
+.setFooter(`WARNED By ${message.author.tag}`,`${message.author.avatarURL}`)   
+.setAuthor('Warn', 'https://cdn3.iconfinder.com/data/icons/musthave/256/Delete.png')
+.setThumbnail(user.avatarURL)
+      .setDescription(` resson :  **${args.join("  ")}**`)
+      .setColor(RANDOM);
+    user.sendEmbed(warn);
+    message.delete();
+    message.channel.send("**Done **")
+  }
+});
+//by ! Angel,  H2#7353 
+//في حال لم ترد ان تحذف رساله الشخص الذي يعطي الوارن فقط قم بحذف   message.delete();
+
  
  
 client.login(process.env.TOKEN);
